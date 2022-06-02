36c49e2b60af6d4d382f460edc17bad4d9df39ad
David <david.dinh.nola@gmail.com>
2022-06-01 22:29:46 -0500

    Added entries

--- food-blog/src/Home.js
+++ food-blog/src/Home.js
@@ -1,22 +1,20 @@
 import {useState} from 'react';
 
 const Home = () => {
-    
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25)
-
-    const handleClick = () => {
-        setName('luigi');
-        setAge(30);
-    }
-
-    
+    const [blogs, setBlogs] = useState([
+        {name: 'Mambo\'s', stars: 4, price: 2, location: 'French Quarter', id: 1},
+        {name: 'Commander\'s Palace', stars: 5, price: 4, location: 'Garden District', id: 2},
+        {name: 'Oceana Grill', stars: 4, price: 2, location: 'French Quarter', id: 3}
+    ]);
     
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map((entry) => (
+                <div className="entry-preview" key={entry.id}>
+                    <h2>{entry.name}</h2>
+                    <p>Located in {entry.location}</p>
+                </div>
+            ))}
         </div>
     );
 }
