ab553f7a2b2313a32878ccc3ed4122d437e7de50
David <david.dinh.nola@gmail.com>
2022-06-03 17:30:03 -0500

    fetching data

--- food-blog/src/Home.js
+++ food-blog/src/Home.js
@@ -2,11 +2,8 @@ import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {name: 'Mambo\'s', stars: 4, price: 2, location: 'French Quarter', id: 1},
-        {name: 'Commander\'s Palace', stars: 5, price: 4, location: 'Garden District', id: 2},
-        {name: 'Oceana Grill', stars: 4, price: 2, location: 'French Quarter', id: 3}
-    ]);
+    const [blogs, setBlogs] = useState(null);
+
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -14,12 +11,19 @@ const Home = () => {
     }
     
     useEffect(() => {
-        console.log("useEffect ran");
-    });
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then((data) => {
+                console.log(data)
+                setBlogs(data)
+            })
+    }, []);
 
     return (
         <div className="home">
-            <BlogList entries={blogs} title="All Restaurants" handleDelete={handleDelete}/>
+            {blogs && <BlogList entries={blogs} title="All Restaurants" handleDelete={handleDelete}/>}
         </div>
     );
 }
