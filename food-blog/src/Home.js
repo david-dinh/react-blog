5f6625cbab614ed55dd277ced5a5c7e7d28fc882
David <david.dinh.nola@gmail.com>
2022-06-03 18:05:31 -0500

    custom hooks

--- food-blog/src/Home.js
+++ food-blog/src/Home.js
@@ -1,36 +1,15 @@
 import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-    
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if(!res.ok) {
-                    throw Error('Could not fetch data');
-                }
-                return res.json();
-            })
-            .then((data) => {
-                setIsPending(false);
-                setBlogs(data);
-                setError(null);
-            })
-            .catch(err => {
-                setIsPending(false);
-                setError(err.message);
-                console.log({error});
-            });
-    }, []);
+    const {data, isPending, error} = useFetch('http://localhost:8000/blogs');
 
-    return (
+    return (   
         <div className="home">
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
-            {blogs && <BlogList entries={blogs} title="All Restaurants"/>}
+            {data && <BlogList entries={data} title="All Restaurants"/>}
         </div>
     );
 }
