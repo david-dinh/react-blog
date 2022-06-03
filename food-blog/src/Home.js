22aa135886758a016efcad1c62bb88a4705a83ff
David <david.dinh.nola@gmail.com>
2022-06-02 23:15:20 -0500

    useEffect hook

--- food-blog/src/Home.js
+++ food-blog/src/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -13,6 +13,10 @@ const Home = () => {
         setBlogs(newBlogs)
     }
     
+    useEffect(() => {
+        console.log("useEffect ran");
+    });
+
     return (
         <div className="home">
             <BlogList entries={blogs} title="All Restaurants" handleDelete={handleDelete}/>
