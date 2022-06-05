321aba73fa99887424893c543707337c2be7add6
David <david.dinh.nola@gmail.com>
2022-06-04 23:57:41 -0500

    abort fetch

--- food-blog/src/useFetch.js
+++ food-blog/src/useFetch.js
@@ -6,23 +6,35 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
     
     useEffect(() => {
-        fetch(url)
-            .then(res => {
-                if(!res.ok) {
-                    throw Error('Could not fetch data');
-                }
-                return res.json();
-            })
-            .then((data) => {
+        const abortCont = new AbortController();
+        
+        setTimeout(() => {
+          fetch(url, {signal: abortCont.signal})
+          .then(res => {
+            if (!res.ok) { // error coming back from server
+              throw Error('could not fetch the data for that resource');
+            } 
+            return res.json();
+          })
+          .then(data => {
+            setIsPending(false);
+            setData(data);
+            setError(null);
+          })
+          .catch(err => {
+            if(err.name === 'AbortError') {
+                console.log('fetch aborted');
+            }
+            else {
                 setIsPending(false);
-                setData(data);
-                setError(null);
-            })
-            .catch(err => {
-                setIsPending(false);
-                setError(err.message);
-            });
-    }, [url]);
+            setError(err.message);
+            }
+          })
+        }, 1000);
+
+        return () => abortCont.abort()
+
+      }, [url]);
 
     return {data, isPending, error};
 }
