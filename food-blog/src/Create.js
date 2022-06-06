bf228402ac2ab722e3efe24b300420d671da3902
David <david.dinh.nola@gmail.com>
2022-06-05 23:21:44 -0500

    controlled forms

--- food-blog/src/Create.js
+++ food-blog/src/Create.js
@@ -1,7 +1,53 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [name, setName] = useState('');
+    const [starsCount, setStarsCount] = useState('1');
+    const [priceCount, setPriceCount] = useState('1');
+    const [location, setLocation] = useState('');
+    
     return (
         <div className="create">
             <h2>Add a new Restaurant</h2>
+            <form>
+                <label>Restaurant Name:</label>
+                <input
+                    type="text"
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <label>Stars:</label>
+                <select
+                    value={starsCount}
+                    onChange= {(e) => setStarsCount(e.target.value)}
+                >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <label>Price:</label>
+                <select
+                    value={starsCount}
+                    onChange= {(e) => setPriceCount(e.target.value)}
+                >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <label>Location:</label>
+                <input
+                    type="text"
+                    required
+                    value={location}
+                    onChange={(e) => setLocation(e.target.value)}
+                />
+                <button>Add Restuarant</button>
+            </form>
         </div>
     );
 }
