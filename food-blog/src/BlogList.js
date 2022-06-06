0562134694ff9e6bda75d8fb09952dfb88641f74
David <david.dinh.nola@gmail.com>
2022-06-05 22:41:47 -0500

    Route Parameters

--- food-blog/src/BlogList.js
+++ food-blog/src/BlogList.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({entries, title}) => {
     return (
         <div className="entry-list">
             <h2>{title}</h2>
             {entries.map((entry) => (
                 <div className="entry-preview" key={entry.id}>
+                    <Link to={`/blogs/${entry.id}`}>
                     <h2>{entry.name}</h2>
                     <p>Located in {entry.location}</p>
+                    </Link>
                 </div>
             ))}
         </div>
