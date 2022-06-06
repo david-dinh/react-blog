1a9832e5b953d48fd365cf01611692f3adc402e8
David <david.dinh.nola@gmail.com>
2022-06-05 23:04:44 -0500

    individual fetches

--- food-blog/src/BlogDetails.js
+++ food-blog/src/BlogDetails.js
@@ -1,11 +1,22 @@
 import { useParams } from "react-router-dom";
+import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const {id} = useParams()
+    const {data, isPending, error} = useFetch(`http://localhost:8000/blogs/${id}`);
     
     return (
         <div className="blog-details">
-            <h2>Blog details - {id}</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {data && (
+                <article>
+                    <h2>{data.name}</h2>
+                    <p>Located in {data.location}</p>
+                    <p>{"⭐".repeat(data.stars)}</p>
+                    <p>{"💲".repeat(data.price)}</p>
+                </article>
+            )}
         </div>
     );
 }
