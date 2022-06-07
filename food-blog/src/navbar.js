10728df4559de61d12dcef1643a14fb00808dcb4
David <david.dinh.nola@gmail.com>
2022-06-06 22:14:01 -0500

    404 page

--- food-blog/src/navbar.js
+++ food-blog/src/navbar.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>The Yummy Blog</h1>
+            <h1><Link to="/" className='title'>The Yummy Blog</Link></h1>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/create">New Blog</Link>
